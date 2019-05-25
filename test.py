30d4614520042efe4a407cb40b87069be1b7be40
huii <sihui@Sihuis-MacBook-Pro.local>
2019-05-26 00:10:38 +0800

    added py file

---
+++ test.py
@@ -0,0 +1,25 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+updater = Updater(token='887166887:AAG8EZo--CWg1KhkwxSbe_Dg_jRcJ8DJ4Po')
+
+
+def start(bot,update):
+    update.message.reply_text("A bot here. Nice to meet you")
+
+def main():
+    # Create Updater object and attach dispatcher to it
+    updater = Updater(TOKEN)
+    dispatcher = updater.dispatcher
+    print("Bot started")
+
+    # Add command handler to dispatcher
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    # Start the bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C
+    updater.idle()
+
+if __name__ == '__main__':
+  main()
